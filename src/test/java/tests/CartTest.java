a24edc7e32b0b1fddf6d177d6a1c5921c7b85dd3
Elizaveta <elizabeth.frisky@gmail.com>
2025-02-06 14:05:34 +0300

    added CartTest (deleting all items) and CartStep

---
+++ src/test/java/tests/CartTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import lombok.extern.log4j.Log4j2;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tests.base.BaseTest;
+
+@Log4j2
+public class CartTest extends BaseTest {
+
+    @Owner("Elizaveta Nikolaenya")
+    @Test(testName = "check Delete All Items From Cart", description = "Deleting all tires from cart.")
+    @Description("Deleting all tires from cart.")
+    public void checkDeleteAllItemsFromCart() {
+        homeStep.openHomePage();
+        loginStep.login(user, password)
+                .changeLanguageToEn();
+        cartStep.deleteAllTiresFromCart();
+
+        String cartIsEmpty = cartPage.cartIsEmpty();
+        Assert.assertEquals(cartIsEmpty, "Your cart is empty", "Expected error message was not " +
+                "displayed.");
+    }
+}
